57b2fd7b18aba0c6f285754c52ba3f3d704f270d
Jorge Gimeno <jgimenolatas@gmail.com>
2024-02-22 10:13:58 +0100

    "Starting Firebase"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,6 +27,11 @@ android {
             )
         }
 
+        getByName("debug"){
+            applicationIdSuffix=".debug"
+            isDebuggable=true
+        }
+
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +43,10 @@ android {
 }
 
 dependencies {
+
+    // FIREBASE
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-analytics")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
